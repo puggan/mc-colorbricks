bca30498b06f117747286282c5a35766fb17f2cb
Thorbjörn Sundragon <puggan@spiro.se>
2020-07-11 16:34:35 +0200

    Add all full blocks

--- src/main/java/se/puggan/colorbricks/items/ColorBricksItem.java
+++ src/main/java/se/puggan/colorbricks/items/ColorBricksItem.java
@@ -12,6 +12,66 @@ public class ColorBricksItem extends BlockItem {
         super(block.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS));
     }
 
+    public static ColorBricksItem black() {
+        return new ColorBricksItem(RegistryHandler.BLACK_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem blue() {
+        return new ColorBricksItem(RegistryHandler.BLUE_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem brown() {
+        return new ColorBricksItem(RegistryHandler.BROWN_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem cyan() {
+        return new ColorBricksItem(RegistryHandler.CYAN_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem gray() {
+        return new ColorBricksItem(RegistryHandler.GRAY_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem green() {
+        return new ColorBricksItem(RegistryHandler.GREEN_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem light_blue() {
+        return new ColorBricksItem(RegistryHandler.LIGHT_BLUE_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem light_gray() {
+        return new ColorBricksItem(RegistryHandler.LIGHT_GRAY_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem lime() {
+        return new ColorBricksItem(RegistryHandler.LIME_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem magenta() {
+        return new ColorBricksItem(RegistryHandler.MAGENTA_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem orange() {
+        return new ColorBricksItem(RegistryHandler.ORANGE_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem pink() {
+        return new ColorBricksItem(RegistryHandler.PINK_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem purple() {
+        return new ColorBricksItem(RegistryHandler.PURPLE_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem red() {
+        return new ColorBricksItem(RegistryHandler.RED_BRICKS_BLOCK);
+    }
+
+    public static ColorBricksItem white() {
+        return new ColorBricksItem(RegistryHandler.WHITE_BRICKS_BLOCK);
+    }
+
     public static ColorBricksItem yellow() {
         return new ColorBricksItem(RegistryHandler.YELLOW_BRICKS_BLOCK);
     }
--- src/main/java/se/puggan/colorbricks/utils/RegistryHandler.java
+++ src/main/java/se/puggan/colorbricks/utils/RegistryHandler.java
@@ -16,7 +16,38 @@ public class RegistryHandler {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, se.puggan.colorbricks.ColorBricks.MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, se.puggan.colorbricks.ColorBricks.MOD_ID);
 
+    public static final RegistryObject<Block> BLACK_BRICKS_BLOCK = BLOCKS.register("black_bricks", ColorBricksBlock::black);
+    public static final RegistryObject<Block> BLUE_BRICKS_BLOCK = BLOCKS.register("blue_bricks", ColorBricksBlock::blue);
+    public static final RegistryObject<Block> BROWN_BRICKS_BLOCK = BLOCKS.register("brown_bricks", ColorBricksBlock::brown);
+    public static final RegistryObject<Block> CYAN_BRICKS_BLOCK = BLOCKS.register("cyan_bricks", ColorBricksBlock::cyan);
+    public static final RegistryObject<Block> GRAY_BRICKS_BLOCK = BLOCKS.register("gray_bricks", ColorBricksBlock::gray);
+    public static final RegistryObject<Block> GREEN_BRICKS_BLOCK = BLOCKS.register("green_bricks", ColorBricksBlock::green);
+    public static final RegistryObject<Block> LIGHT_BLUE_BRICKS_BLOCK = BLOCKS.register("light_blue_bricks", ColorBricksBlock::light_blue);
+    public static final RegistryObject<Block> LIGHT_GRAY_BRICKS_BLOCK = BLOCKS.register("light_gray_bricks", ColorBricksBlock::light_gray);
+    public static final RegistryObject<Block> LIME_BRICKS_BLOCK = BLOCKS.register("lime_bricks", ColorBricksBlock::lime);
+    public static final RegistryObject<Block> MAGENTA_BRICKS_BLOCK = BLOCKS.register("magenta_bricks", ColorBricksBlock::magenta);
+    public static final RegistryObject<Block> ORANGE_BRICKS_BLOCK = BLOCKS.register("orange_bricks", ColorBricksBlock::orange);
+    public static final RegistryObject<Block> PINK_BRICKS_BLOCK = BLOCKS.register("pink_bricks", ColorBricksBlock::pink);
+    public static final RegistryObject<Block> PURPLE_BRICKS_BLOCK = BLOCKS.register("purple_bricks", ColorBricksBlock::purple);
+    public static final RegistryObject<Block> RED_BRICKS_BLOCK = BLOCKS.register("red_bricks", ColorBricksBlock::red);
+    public static final RegistryObject<Block> WHITE_BRICKS_BLOCK = BLOCKS.register("white_bricks", ColorBricksBlock::white);
     public static final RegistryObject<Block> YELLOW_BRICKS_BLOCK = BLOCKS.register("yellow_bricks", ColorBricksBlock::yellow);
+
+    public static final RegistryObject<Item> BLACK_BRICKS_ITEM = ITEMS.register("black_bricks", ColorBricksItem::black);
+    public static final RegistryObject<Item> BLUE_BRICKS_ITEM = ITEMS.register("blue_bricks", ColorBricksItem::blue);
+    public static final RegistryObject<Item> BROWN_BRICKS_ITEM = ITEMS.register("brown_bricks", ColorBricksItem::brown);
+    public static final RegistryObject<Item> CYAN_BRICKS_ITEM = ITEMS.register("cyan_bricks", ColorBricksItem::cyan);
+    public static final RegistryObject<Item> GRAY_BRICKS_ITEM = ITEMS.register("gray_bricks", ColorBricksItem::gray);
+    public static final RegistryObject<Item> GREEN_BRICKS_ITEM = ITEMS.register("green_bricks", ColorBricksItem::green);
+    public static final RegistryObject<Item> LIGHT_BLUE_BRICKS_ITEM = ITEMS.register("light_blue_bricks", ColorBricksItem::light_blue);
+    public static final RegistryObject<Item> LIGHT_GRAY_BRICKS_ITEM = ITEMS.register("light_gray_bricks", ColorBricksItem::light_gray);
+    public static final RegistryObject<Item> LIME_BRICKS_ITEM = ITEMS.register("lime_bricks", ColorBricksItem::lime);
+    public static final RegistryObject<Item> MAGENTA_BRICKS_ITEM = ITEMS.register("magenta_bricks", ColorBricksItem::magenta);
+    public static final RegistryObject<Item> ORANGE_BRICKS_ITEM = ITEMS.register("orange_bricks", ColorBricksItem::orange);
+    public static final RegistryObject<Item> PINK_BRICKS_ITEM = ITEMS.register("pink_bricks", ColorBricksItem::pink);
+    public static final RegistryObject<Item> PURPLE_BRICKS_ITEM = ITEMS.register("purple_bricks", ColorBricksItem::purple);
+    public static final RegistryObject<Item> RED_BRICKS_ITEM = ITEMS.register("red_bricks", ColorBricksItem::red);
+    public static final RegistryObject<Item> WHITE_BRICKS_ITEM = ITEMS.register("white_bricks", ColorBricksItem::white);
     public static final RegistryObject<Item> YELLOW_BRICKS_ITEM = ITEMS.register("yellow_bricks", ColorBricksItem::yellow);
 
     public static void init() {
